cede0d70ba8900d17449228ade9b3107ddc101c9
weslei bruno de santana <weslei945@gmail.com>
2023-09-05 10:51:31 -0300

    second commit

---
+++ README.md
@@ -0,0 +1,8 @@
+### iniciando projeto
+`npm init -y`
+
+### instalação do express
+`npm i express`
+
+### instalação nodemon
+`npm i nodemon`
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello world")
+const express = require('express')
+const app = express()
+
+app.get('/home', function (req, res) {
+  res.send('Home')
+})
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+  })
+
+app.listen(3000)
